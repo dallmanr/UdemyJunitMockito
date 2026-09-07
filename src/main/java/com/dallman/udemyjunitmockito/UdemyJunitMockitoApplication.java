2f94462c33094eaa75b74a6deb7ececd6dec5ffa
Richard Dallman <dallmanr@users.noreply.github.com>
2026-09-07 09:41:35 +0100

    Init commit
    * Project creation

---
+++ src/main/java/com/dallman/udemyjunitmockito/UdemyJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.dallman.udemyjunitmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyJunitMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyJunitMockitoApplication.class, args);
+    }
+
+}
