558d9be1fa624c1e5eea49f12466b5e3bdf618aa
Richard Dallman <dallmanr@users.noreply.github.com>
2026-09-10 14:24:16 +0100

    Csv, File and MethodSource test data
    * Added Parametrized tests that include Csv, CsvFile and method returning a 2d array of values

--- src/test/java/com/dallman/udemyjunitmockito/FizzBuzzTest.java
+++ src/test/java/com/dallman/udemyjunitmockito/FizzBuzzTest.java
@@ -1,6 +1,11 @@
 package com.dallman.udemyjunitmockito;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 //Test driven development example
@@ -39,6 +44,42 @@ class FizzBuzzTest {
     void testNotDivisibleByFifteen() {
         String expected = "16";
         assertEquals(expected, FizzBuzz.compute(16), "Should return n");
+    }
+
+    @ParameterizedTest(name="value={0}, expected={1}")
+    @DisplayName("Testing with CSV data")
+    @CsvSource({"1,1", "2,2", "3,Fizz", "4,4", "5,Buzz", "6, Fizz", "7,7" }) // "value,expected"
+    @Order(6)
+    void testWithCSVData(int value, String expected) {
+        assertEquals(expected,  FizzBuzz.compute(value));
+    }
+
+    @ParameterizedTest(name="value={0}, expected={1}")
+    @DisplayName("Testing with test-data.csv")
+    @CsvFileSource(resources="/test-data.csv")
+    @Order(6)
+    void testWithCsvFile(int value, String expected) {
+        assertEquals(expected,  FizzBuzz.compute(value));
+    }
+
+    @ParameterizedTest(name="value={0}, expected={1}")
+    @DisplayName("Testing with method source")
+    @MethodSource("testData")
+    @Order(6)
+    void testWithmethodSource(int value, String expected) {
+        assertEquals(expected,  FizzBuzz.compute(value));
+    }
+
+    static Object[][] testData() {
+        return new Object[][] {
+                {1, "1"},
+                {2, "2"},
+                {3, "Fizz"},
+                {4, "4"},
+                {5, "Buzz"},
+                {6, "Fizz"},
+                {7, "7"}
+        };
     }
 
 }
