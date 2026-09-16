a25769e5d859879657a442473584fc1ae4a61c97
Richard Dallman <dallmanr@users.noreply.github.com>
2026-09-16 13:34:51 +0100

    Project setup
    * Initial project setup for Mock

---
+++ src/main/java/com/dallman/udemyjunitmockito/service/ApplicationService.java
@@ -0,0 +1,26 @@
+package com.dallman.udemyjunitmockito.service;
+
+import com.dallman.udemyjunitmockito.dao.ApplicationDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+
+public class ApplicationService {
+
+    @Autowired
+    private ApplicationDAO applicationDao;
+
+    public double addGradeResultsForSingleClass(List<Double> numbers) {
+        return applicationDao.addGradeResultsForSingleClass(numbers);
+    }
+
+    public double findGradePointAverage (List<Double> grades ) {
+        return applicationDao.findGradePointAverage(grades);
+    }
+
+    public Object checkNull(Object obj) {
+        return applicationDao.checkNull(obj);
+    }
+
+}
