a24f452f2d76313bc9efa1e50be5a8dbba0b4738
Richard Dallman <dallmanr@users.noreply.github.com>
2026-09-16 16:42:05 +0100

    Exception testing
    * Checking for runtime exceptions in throwAnException
    * Chaining calls in stubConsecutiveCalls so first time we check against the expected exception and 2nd time we check against an expected value

--- src/test/java/com/dallman/udemyjunitmockito/MockAnnotationTest.java
+++ src/test/java/com/dallman/udemyjunitmockito/MockAnnotationTest.java
@@ -13,8 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = UdemyJunitMockitoApplication.class)
@@ -86,6 +85,47 @@ public class MockAnnotationTest {
         assertNotNull(applicationService.checkNull(studentOne.getGrades().getMathGradeResults()), "Object should not be null");
 
         // 3. Verify method was called at least once
-        verify(applicationDAO, times(1)).checkNull(studentGrades.getMathGradeResults());
+        verify(applicationDAO, atLeastOnce()).checkNull(studentGrades.getMathGradeResults());
+    }
+
+    @Test
+    @DisplayName("Throw an exception")
+    void thrownAnException() {
+        // 1. We have manually created a bean in UdemyJunitMockitoApplication with the name of collegeStudent, so retrieve it
+        CollegeStudent nullStudent = (CollegeStudent) applicationContext.getBean("collegeStudent");
+
+        // 2. When we make this call to checkNull, throw a RuntimeException
+        doThrow(new RuntimeException()).when(applicationDAO).checkNull(nullStudent);
+
+        // 3. Assert the exception is thrown
+        assertThrows(RuntimeException.class, () -> {
+            applicationService.checkNull(nullStudent);
+        });
+
+        verify(applicationDAO, atLeastOnce()).checkNull(nullStudent);
+    }
+
+    @Test
+    @DisplayName("Multiple stubing")
+    void stubConsecutiveCalls() {
+        // 1. We have manually created a bean in UdemyJunitMockitoApplication with the name of collegeStudent, so retrieve it
+        CollegeStudent nullStudent = (CollegeStudent) applicationContext.getBean("collegeStudent");
+
+        // 2. Set the expectation. On first call throw a RuntimeException. On subsequent calls, return the string
+        when(applicationDAO.checkNull(nullStudent))
+                .thenThrow(new RuntimeException())
+                .thenReturn("Do not throw a second time");
+
+        // 3.1 Assert - This should result in RuntimeException
+        assertThrows(RuntimeException.class, () -> {
+            applicationService.checkNull(nullStudent);
+        });
+
+        // 3.2 Assert - The subsequent call - Should result in string being returned.
+        // We give it the expected, "Do not throw a second time" and check the return value matches this
+        assertEquals("Do not throw a second time", applicationService.checkNull(nullStudent));
+
+        // 4. Verify the method was called twice
+        verify(applicationDAO, atLeast(2)).checkNull(nullStudent);
     }
 }
